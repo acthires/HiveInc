3173a5be788245eed8713471825af695dc95ecba
kangyanli <kangyanli@hadoop11.(none)>
2016-05-13 16:32:23 +0800

    put query q1.sql

--- TestCases/q1.sql
+++ TestCases/q1.sql
@@ -1,12 +1,20 @@
+set hive.inc.test.findNewFiles=true;
+set hive.inc.test.putNewFiles=true;
+set hive.inc.test.runinc=true;
+set hive.inc.test.runoriginal=false;
 set mapred.min.split.size=536870912;
 set hive.exec.reducers.bytes.per.reducer=1225000000;
 set hive.auto.convert.join=false;
+set hive.inctimes=10;
+set hive.inc.costmodel.gby=true;
+set hive.inc.file.sourcedir=/home/kangyanli/tpch_2_15.0/dbgen/20G/;
+set hive.inc.file.sinkdir=/home/kangyanli/tpch/20G/;
 
 --Q1
 SELECT
   L_RETURNFLAG, L_LINESTATUS, SUM(L_QUANTITY), SUM(L_EXTENDEDPRICE), SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)), SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)*(1+L_TAX)), AVG(L_QUANTITY), AVG(L_EXTENDEDPRICE), AVG(L_DISCOUNT), COUNT(1)
 FROM
-  lineitem
+  lineitem/*+INCREMENTAL(after 2014/3/4,12:5:20 INTERVAL 3/second)*/
 WHERE
   L_SHIPDATE<='1998-09-02'
 GROUP BY L_RETURNFLAG, L_LINESTATUS
