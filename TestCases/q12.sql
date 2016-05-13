0628ac93df565be851b4918c66884fe426c2a38e
kangyanli <kangyanli@hadoop11.(none)>
2016-05-13 16:33:28 +0800

    put query q12.sql

---
+++ TestCases/q12.sql
@@ -0,0 +1,40 @@
+set hive.inc.test.findNewFiles=true;
+set hive.inc.test.putNewFiles=true;
+set hive.inc.test.runinc=true;
+set hive.inc.test.runoriginal=false;
+set mapred.min.split.size=536870912;
+set hive.exec.reducers.bytes.per.reducer=1225000000;
+set hive.auto.convert.join=false;
+set hive.inctimes=10;
+set hive.inc.costmodel.gby=true;
+set hive.inc.file.sourcedir=/home/kangyanli/tpch_2_15.0/dbgen/20G/;
+set hive.inc.file.sinkdir=/home/kangyanli/tpch/20G/;
+
+--Q12
+select 
+  l_shipmode,
+  sum(case
+    when o_orderpriority ='1-URGENT'
+         or o_orderpriority ='2-HIGH'
+    then 1
+    else 0
+end
+  ) as high_line_count,
+  sum(case
+    when o_orderpriority <> '1-URGENT'
+         and o_orderpriority <> '2-HIGH'
+    then 1
+    else 0
+end
+  ) as low_line_count
+from
+  orders o join lineitem/*+INCREMENTAL(2014/3/4,12:5:20-2014/8/4,12:5:20)*/ l 
+  on 
+    o.o_orderkey = l.l_orderkey and l.l_commitdate < l.l_receiptdate
+and l.l_shipdate < l.l_commitdate and l.l_receiptdate >= '1994-01-01' 
+and l.l_receiptdate < '1995-01-01'
+where 
+  l.l_shipmode = 'MAIL' or l.l_shipmode = 'SHIP'
+group by l_shipmode
+order by l_shipmode;
+
