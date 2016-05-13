738c39cde6af0ffbc085359f493d7779182b8819
kangyanli <kangyanli@hadoop11.(none)>
2016-05-13 16:19:23 +0800

    Modify all.sql.

--- TestCases/all.sql
+++ TestCases/all.sql
@@ -6,11 +6,10 @@ set mapred.min.split.size=536870912;
 set hive.exec.reducers.bytes.per.reducer=1225000000;
 set hive.auto.convert.join=false;
 set hive.inctimes=10;
---set hive.inc.split.gby=false;
 set hive.inc.costmodel.gby=true;
---set hive.inc.test.costmodel.split=true;
 set hive.inc.file.sourcedir=/home/kangyanli/tpch_2_15.0/dbgen/20G/;
 set hive.inc.file.sinkdir=/home/kangyanli/tpch/20G/;
+
 --Q1
 SELECT
   L_RETURNFLAG, L_LINESTATUS, SUM(L_QUANTITY), SUM(L_EXTENDEDPRICE), SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)), SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)*(1+L_TAX)), AVG(L_QUANTITY), AVG(L_EXTENDEDPRICE), AVG(L_DISCOUNT), COUNT(1)
